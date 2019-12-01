6c783aff8ae34a8883fbddc7c5c33631ab0b9555
Bogdan Protsenko <bogdan@grabyo.com>
2019-12-01 16:57:06 +0000

    fuck

--- client-collab/src/index.js
+++ client-collab/src/index.js
@@ -9,7 +9,7 @@ import socketIOClient from 'socket.io-client';
 import { fabric } from "fabric";
 
 let isHandlingMessage = false;
-const socket = socketIOClient(`https://178.62.86.47:3001`);
+const socket = socketIOClient(`http://178.62.86.47:3001`);
 const ui = UI.create({});
 const store = Store.create({
   playbackState: {
